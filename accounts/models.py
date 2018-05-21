4b9146f394392e08cd3b278460b250daeb1697cf
handole <denihhandoko@gmail.com>
2018-05-21 14:40:25 +0700

    [develop] add isiprofil on accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profil(models.Model):
+	firstname = models.CharField(max_length=50)
+	lastname = models.CharField(max_length=50)
+	username = models.ForeignKey(User, related_name='username')
+	email = models.EmailField(max_length=100)
+	gender = models.CharField(max_length=30)
+	location = models.CharField(max_length=100)
+	bio = models.TextField()
+
+	def __str__(self):
+		return "%s %s" % (self.lastname, self.firstname)
